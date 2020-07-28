0473ef17c0d4d45cc0cbb7432982f6ed8170941e
leebowden <leebowden22@gmail.com>
2020-07-28 10:29:03 +1000

    Graph 2 and 3 with R Sctipts

---
+++ plot2.R
@@ -0,0 +1,32 @@
+##open URL and download file
+
+fileURL<- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileURL, dest="dataset.zip") 
+data<-unzip ("./dataset.zip", exdir = ".")
+file <- read.table(data,header=T, sep=";", stringsAsFactors = F)
+
+#subset data table to extract relevant data
+
+library(dplyr)
+
+rows1 <- filter(file, file$Date == "1/2/2007")
+rows2 <- filter(file, file$Date == "2/2/2007")
+subsetdata <- rbind(rows1, rows2)
+
+## reformat colums to correct classes to allow plotting 
+
+subsetdata$Date <- as.Date(subsetdata$Date, format = "%d/%m/%y")
+class(subsetdata$Date)
+
+subsetdata$Time <- strptime(subsetdata$Time, format = "%H:%M:%S" )
+class(subsetdata$Time)
+
+subsetdata$Global_active_power <- as.numeric(subsetdata$Global_active_power)
+
+
+## Create plot and save as PNG file
+
+png('plot1.png')
+hist(subsetdata$Global_active_power, col = "Red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+dev.off()
+
---
+++ plot3.R
@@ -0,0 +1,36 @@
+##open URL and download file
+
+fileURL<- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileURL, dest="dataset.zip") 
+data<-unzip ("./dataset.zip", exdir = ".")
+file <- read.table(data,header=T, sep=";", stringsAsFactors = F)
+
+#subset data table to extract relevant data
+
+library(dplyr)
+rows1 <- filter(file, file$Date == "1/2/2007")
+rows2 <- filter(file, file$Date == "2/2/2007")
+subsetdata <- rbind(rows1, rows2)
+
+## Reformat colums to correct classes to allow plotting 
+
+
+subsetdata$Global_active_power <- as.numeric(subsetdata$Global_active_power)
+subsetdata$Sub_metering_1 <- as.numeric(subsetdata$Sub_metering_1)
+subsetdata$Sub_metering_2 <- as.numeric(subsetdata$Sub_metering_2)
+subsetdata$Sub_metering_3 <- as.numeric(subsetdata$Sub_metering_3)
+
+#### Transform date to Date format and create new variable with Date and Time joined
+
+datetime <- strptime(paste(subsetdata$Date, subsetdata$Time, sep=" "),format =  "%d/%m/%Y %H:%M:%S")
+subsetdata<-cbind(subsetdata, "DateTime" = datetime)
+
+## Create plot and save as PNG file
+
+png('plot3.png')
+plot(subsetdata$Sub_metering_1 ~ subsetdata$DateTime, type= "l", col = "Black", xlab = " ", ylab = "Energy sub metering")
+lines(subsetdata$Sub_metering_2 ~ subsetdata$DateTime, type= "l", col = "Red")
+lines(subsetdata$Sub_metering_3 ~ subsetdata$DateTime, type= "l", col = "Blue")
+legend("topright", c("Sub_metering_1" , "Sub_metering_2", "Sub_metering_3"), lty=c(1,1,1), lwd=c(2.5,2.5,2.5),col=c("Black","Red","Blue"))
+dev.off()
+
